4582d7709a64d3b30ef29d614114127912e8c6fc
kevlongalloway <kevlongalloway1999m@gmail.com>
2023-06-17 13:30:32 -0700

    2 files modified

--- src/components/withAuth.tsx
+++ src/components/withAuth.tsx
@@ -2,9 +2,13 @@ import React from 'react';
 import { Redirect } from 'react-router-dom';
 
 export default function withAuth(WrappedComponent) {
-  return function WithAuth(props) {
-    const isAuthenticated = !!localStorage.getItem('access_token'); // Check if access token exists
 
+  
+  
+  return function WithAuth(props) {
+    
+    const isAuthenticated = !!localStorage.getItem('access_token'); // Check if access token exists
+    
     if (isAuthenticated) {
       return <WrappedComponent {...props} />;
     } else {
